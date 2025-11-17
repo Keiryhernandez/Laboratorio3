99a5fb3be06ecb9e897885069ab42d0a06c2f5f5
Keiry <keyri2017hernadez@gmail.com>
2025-11-16 21:11:29 -0600

    first commit

---
+++ Laboratorio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RepoDb;
+using RepoDb.Enumerations;
+
+namespace Laboratorio3
+{
+    
+
+    internal static class Program
+    {
+
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            RepoDb.SqlServerBootstrap.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Laboratorio3/Repositorios/ProductoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3.Repositorios
+{
+    internal class ProductoRepositorio
+    {
+    }
+}
